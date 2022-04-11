5887bd21e2129a5457e7c4c0f5c38b15725a4284
Justin Perdok <justin-p@users.noreply.github.com>
2022-04-11 06:51:28 -0700

    Bump digitalocean version (#2)

--- terraform/digitalocean/project/providers.tf
+++ terraform/digitalocean/project/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "~> 2.12.1"
+      version = "~> 2.19.0"
     }
   }
   required_version = ">= 1.0.8"
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "~> 2.12.1"
+      version = "~> 2.19.0"
     }
     random = {
       source = "hashicorp/random"
