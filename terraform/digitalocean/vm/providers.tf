4a84adff9f00edce8efc990c5894ba5a91863650
Justin Perdok <justin-p@users.noreply.github.com>
2022-04-11 15:39:48 +0200

    Update providers.tf

--- terraform/digitalocean/vm/providers.tf
+++ terraform/digitalocean/vm/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "~> 2.12.1"
+      version = "~> 2.19.0"
     }
     random = {
       source = "hashicorp/random"
